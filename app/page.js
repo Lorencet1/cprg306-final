56a1b2b67814f5a88286c030a512334b1305ac53
Lorencet1 <Lorence.talay@edu.sait.ca>
2024-04-02 00:53:59 -0600

    Your commit message here

--- app/page.js
+++ app/page.js
@@ -1,28 +1,31 @@
 import React from 'react';
+import Image from 'next/image';
 
 const HomePage = () => {
     return (
-        <div className=" flex flex-col items-start justify-center min-h-screen ml-20 bg-gradient-to-r from-black to-gray-300">
-            <div className="flex justify-between w-full mt-11">
+        <div className="flex flex-col items-start justify-center min-h-screen ml-20 bg-gradient-to-r from-black to-gray-300">
+            <div className="flex justify-between w-full mt-20">
                 <header className="flex-grow mt-20">
-                    <h1 className='text-7xl'>Hi, I'm Lorence</h1>
+                    <h1 className='text-7xl text-white'>Hi, I'm Lorence</h1>
                     <br></br>
-                    <p className='text-3xl'>Welcome to my portfolio</p>
+                    <p className='text-3xl text-white'>Welcome to my portfolio</p>
                 </header>
 
                 <div className="flex flex-col items-center mr-20 text-black mt-20">
                     <h1 className='text-7xl'>&lt;Coder&gt;</h1>
-                    <p className=" text-2xl mt-8 text-1xl max-w-md mx-auto text-center">
+                    <p className="text-2xl mt-8 max-w-md mx-auto text-center">
                         An aspiring front-end developer dedicated to crafting standout, user-friendly digital experiences.
                     </p>
                 </div>
             </div>
 
             <div className="flex justify-center w-full">
-                <img
-                    src="../myGif/linux-computer.gif" 
+                <Image
+                    src="/myGif/linux-computer.gif"
                     alt="Linux Computer"
-                    style={{ maxWidth: '400px', height: '400px' }} 
+                    width={400}
+                    height={400}
+                    layout="intrinsic"
                 />
             </div>
         </div>
