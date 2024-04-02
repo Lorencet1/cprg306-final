881f4b480d8022ec482b05cca92487d00de40456
Lorencet1 <Lorence.talay@edu.sait.ca>
2024-04-02 01:02:48 -0600

    Your commit message here

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ const HomePage = () => {
         <div className="flex flex-col items-start justify-center min-h-screen ml-20 bg-gradient-to-r from-black to-gray-300">
             <div className="flex justify-between w-full mt-20">
                 <header className="flex-grow mt-20">
-                    <h1 className='text-7xl text-white'>Hi, I'm Lorence</h1>
+                    <h1 className='text-7xl text-white'>Hi, I&apos;m Lorence</h1>
                     <br></br>
                     <p className='text-3xl text-white'>Welcome to my portfolio</p>
                 </header>
